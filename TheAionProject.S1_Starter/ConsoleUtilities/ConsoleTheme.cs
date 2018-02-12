98d7958d7406a0dff1ec79a2dfced34bb75372de
KeeganAles <ales2@mail.nmc.edu>
2018-02-12 17:08:16 -0500

    Add project files.

---
+++ TheAionProject.S1_Starter/ConsoleUtilities/ConsoleTheme.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheAionProject
+{
+    /// <summary>
+    /// static class to manage the console game theme
+    /// </summary>
+    public static class ConsoleTheme
+    {
+        //
+        // splash screen colors
+        //
+        public static ConsoleColor SplashScreenBackgroundColor = ConsoleColor.Black;
+        public static ConsoleColor SplashScreenForegroundColor = ConsoleColor.Green;
+
+        //
+        // main console window colors
+        //
+        public static ConsoleColor WindowBackgroundColor = ConsoleColor.Black;
+        public static ConsoleColor WindowForegroundColor = ConsoleColor.White;
+
+        //
+        // console window header colors
+        //
+        public static ConsoleColor HeaderBackgroundColor = ConsoleColor.DarkBlue;
+        public static ConsoleColor HeaderForegroundColor = ConsoleColor.Green;
+
+        //
+        // console window footer colors
+        //
+        public static ConsoleColor FooterBackgroundColor = ConsoleColor.DarkBlue;
+        public static ConsoleColor FooterForegroundColor = ConsoleColor.Green;
+
+        //
+        // menu box colors
+        //
+        public static ConsoleColor MenuBackgroundColor = ConsoleColor.Black;
+        public static ConsoleColor MenuForegroundColor = ConsoleColor.Red;
+        public static ConsoleColor MenuBorderColor = ConsoleColor.DarkBlue;
+
+        //
+        // message box colors
+        //
+        public static ConsoleColor MessageBoxBackgroundColor = ConsoleColor.Black;
+        public static ConsoleColor MessageBoxForegroundColor = ConsoleColor.Red;
+        public static ConsoleColor MessageBoxBorderColor = ConsoleColor.DarkBlue;
+        public static ConsoleColor MessageBoxHeaderBackgroundColor = ConsoleColor.Black;
+        public static ConsoleColor MessageBoxHeaderForegroundColor = ConsoleColor.Green;
+
+        //
+        // status box colors
+        //
+        public static ConsoleColor StatusBoxBackgroundColor = ConsoleColor.Black;
+        public static ConsoleColor StatusBoxForegroundColor = ConsoleColor.Red;
+        public static ConsoleColor StatusBoxBorderColor = ConsoleColor.DarkBlue;
+        public static ConsoleColor StatusBoxHeaderBackgroundColor = ConsoleColor.Black;
+        public static ConsoleColor StatusBoxHeaderForegroundColor = ConsoleColor.Green;
+
+        //
+        // input box colors
+        //
+        public static ConsoleColor InputBoxBackgroundColor = ConsoleColor.Black;
+        public static ConsoleColor InputBoxForegroundColor = ConsoleColor.Red;
+        public static ConsoleColor InputBoxErrorMessageForegroundColor = ConsoleColor.Yellow;
+        public static ConsoleColor InputBoxBorderColor = ConsoleColor.DarkBlue;
+        public static ConsoleColor InputBoxHeaderBackgroundColor = ConsoleColor.Black;
+        public static ConsoleColor InputBoxHeaderForegroundColor = ConsoleColor.Green;
+    }
+}
